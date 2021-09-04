9566a9d41c90732be63866a1a8d5bb4223665b43
Suzin <patrycjusz.sosnowski@gmail.com>
2021-09-04 20:39:47 +0200

    dodanie elementów z logowaniem do navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,21 @@
             <a class="p-2 text-dark" href="{{ route('home.contact') }}">Contact</a>
             <a class="p-2 text-dark" href="{{ route('posts.index') }}">Blog Posts</a>
             <a class="p-2 text-dark" href="{{ route('posts.create') }}">Add blog post</a>
+
+            @guest
+                @if(Route::has('register'))
+                    <a class="p-2 text-dark" href="{{ route('register') }}">Register</a>
+                @endif
+                <a class="p-2 text-dark" href="{{ route('login') }}">Login</a>
+            @else
+                <a class="p-2 text-dark" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();document.getElementById('logout-form').submit()">
+                    Logout ({{(Auth::getUser())->name}})
+                </a>
+                <form action="{{route('logout')}}" id="logout-form" method="POST" style="display: none">
+                    @csrf
+                </form>
+            @endguest
         </nav>
     </div>
     <div class="container">
