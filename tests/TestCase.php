135b7721ff36811a053815b82878d83359e417e2
Suzin <patrycjusz.sosnowski@gmail.com>
2021-09-04 20:40:35 +0200

    add user method to test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function user()
+    {
+
+        return User::factory()->create();
+    }
 }
