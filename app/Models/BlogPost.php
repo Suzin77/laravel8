c0d835da521fd61582c5dd7f08c3228b93c51420
Suzin <patrycjusz.sosnowski@gmail.com>
2021-09-16 10:45:06 +0200

    dodanie scope by admin widzial soft deleted

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\DeletedAdminScope;
 use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -21,6 +22,7 @@ class BlogPost extends Model
 
     public static function boot()
     {
+        static::addGlobalScope(new DeletedAdminScope);
         parent::boot();
 
         //static::addGlobalScope(new LatestScope);
